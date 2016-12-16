e934a8b6af0fbb1daefe050a1284ba2eaf9b5b41
Tom Owsiak <towsiak@yahoo.com>
2016-12-16 11:43:29 -0500

    update lesson 63

---
+++ WindowsFormsApplication8/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApplication8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWithFreezing = new System.Windows.Forms.Button();
+            this.btnNoFreezing = new System.Windows.Forms.Button();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnWithFreezing
+            // 
+            this.btnWithFreezing.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnWithFreezing.Location = new System.Drawing.Point(13, 13);
+            this.btnWithFreezing.Name = "btnWithFreezing";
+            this.btnWithFreezing.Size = new System.Drawing.Size(780, 55);
+            this.btnWithFreezing.TabIndex = 0;
+            this.btnWithFreezing.Text = "Download With Freezing";
+            this.btnWithFreezing.UseVisualStyleBackColor = true;
+            this.btnWithFreezing.Click += new System.EventHandler(this.btnWithFreezing_Click);
+            // 
+            // btnNoFreezing
+            // 
+            this.btnNoFreezing.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnNoFreezing.Location = new System.Drawing.Point(12, 74);
+            this.btnNoFreezing.Name = "btnNoFreezing";
+            this.btnNoFreezing.Size = new System.Drawing.Size(780, 55);
+            this.btnNoFreezing.TabIndex = 1;
+            this.btnNoFreezing.Text = "Download Without Freezing";
+            this.btnNoFreezing.UseVisualStyleBackColor = true;
+            this.btnNoFreezing.Click += new System.EventHandler(this.btnNoFreezing_Click);
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.btnOpenFile.Location = new System.Drawing.Point(13, 135);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(780, 55);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "Open File In Windows Explorer";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 578);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.btnNoFreezing);
+            this.Controls.Add(this.btnWithFreezing);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWithFreezing;
+        private System.Windows.Forms.Button btnNoFreezing;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApplication8/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;//needed to make a web client object
+using System.Diagnostics;//needed to make use of PRocess.Start
+
+namespace WindowsFormsApplication8
+{
+    public partial class Form1 : Form
+    {
+        WebClient client = new WebClient();//declare and set a new client object
+        public Form1()//constructor, it runs when the form is made 
+        {
+            InitializeComponent();
+        }
+
+        private void btnWithFreezing_Click(object sender, EventArgs e)
+        {
+            //DownloadFile has the effect of freezing the interface while the download occurs
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                client.DownloadFile("https://pixabay.com/en/videos/download/video-5194_source.mp4", saveFileDialog1.FileName);
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe", "/select," + saveFileDialog1.FileName);//open windows explorer with file selected or highlighted
+        }
+
+        private void btnNoFreezing_Click(object sender, EventArgs e)
+        {
+            //DownloadFileAsync has the effect of downloading the file in the background while the program remains usable
+            //the interface does not freeze during the download
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                client.DownloadFileAsync(new Uri("https://pixabay.com/en/videos/download/video-5194_source.mp4"), saveFileDialog1.FileName);
+        }
+    }
+}
